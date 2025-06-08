a9e0fff4628b5523b1c6e160007acd88e5ecc164
Patrick Callahan <patricktcallahan18@gmail.com>
2025-06-08 17:51:19 +0200

    add unit tests for TextNode class and update TextType enum

---
+++ src/test_textnode.py
@@ -0,0 +1,38 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+    def test_eq_normal(self):
+        node = TextNode("This is a text node", TextType.NORMAL)
+        node2 = TextNode("This is a text node", TextType.NORMAL)
+        self.assertEqual(node, node2)
+    def test_eq_italic(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertEqual(node, node2)
+    def test_eq_code(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        node2 = TextNode("This is a text node", TextType.CODE)
+        self.assertEqual(node, node2)
+    def test_eq_link(self):
+        node = TextNode("This is a text node", TextType.LINK, url="https://example.com")
+        node2 = TextNode("This is a text node", TextType.LINK, url="https://example.com")
+        self.assertEqual(node, node2)
+    def test_eq_no_link(self):
+        node = TextNode("This is a text node", TextType.LINK, url=None)
+        node2 = TextNode("This is a text node", TextType.LINK, url=None)
+        self.assertEqual(node, node2)
+    def test_eq_image(self):
+        node = TextNode("This is a text node", TextType.IMAGE)
+        node2 = TextNode("This is a text node", TextType.IMAGE)
+        self.assertEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
