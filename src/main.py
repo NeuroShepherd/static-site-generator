396ebd6f6b250fe0b5daa750e58a98050ca13043
Patrick Callahan <patricktcallahan18@gmail.com>
2025-06-11 14:17:58 +0200

    Set up website generation steps

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
 print("hello world")
 from textnode import TextNode, TextType
-from helpers import source_to_dest
+from helpers import (
+    source_to_dest,
+    generate_page,
+    clear_public_dir,
+)
+import os
 
 def main():
-    text_node = TextNode("This is some anchor text", "link", "https://www.boot.dev")
-    print(text_node.__repr__())
+    # text_node = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    # print(text_node.__repr__())
+    clear_public_dir()
     source_to_dest("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 if __name__ == "__main__":
     main()
